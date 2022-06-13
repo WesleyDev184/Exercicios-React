a1838a0d16e47e241793b73d98cb66385c36aaa8
WesleyDev <57929638+WesleyDev184@users.noreply.github.com>
2022-06-13 15:15:31 -0300

    Resolvido

---
+++ exercicio-1/src/components/lorem.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+function Lorem (){
+    return(
+        <p>Mussum Ipsum, cacilds vidis litro abertis. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Si num tem leite então bota uma pinga aí cumpadi!Aenean aliquam molestie leo, vitae iaculis nisl.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.</p>
+    )
+}
+
+export default Lorem;
