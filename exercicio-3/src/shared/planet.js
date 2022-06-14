03dc9ba86ee6d7e48629363d463c5af02a6caf82
WesleyDev <57929638+WesleyDev184@users.noreply.github.com>
2022-06-14 15:00:57 -0300

    formatação

--- exercicio-3/src/shared/planet.js
+++ exercicio-3/src/shared/planet.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import DescriptionWithLink from './descriptioWithLink';
+import React from "react";
+import DescriptionWithLink from "./descriptioWithLink";
 
-
-async function getSatellites(id){
-  let response = await fetch(`http://localhost:3000/api/${id}.json`)
-  let date = await response.json()
+async function getSatellites(id) {
+  let response = await fetch(`http://localhost:3000/api/${id}.json`);
+  let date = await response.json();
   return date;
 }
 
@@ -12,35 +11,38 @@ class Planet extends React.Component {
   constructor(props) {
     super(props);
   }
-  state = { 
-    satellites:[]
-   }
+  state = {
+    satellites: [],
+  };
 
   componentDidMount() {
-    getSatellites(this.props.id).then(data =>{
-      this.setState(state =>({
-        satellites: data['satellites']
-      }))
-    })
+    getSatellites(this.props.id).then((data) => {
+      this.setState((state) => ({
+        satellites: data["satellites"],
+      }));
+    });
   }
 
-  render() { 
-    return ( 
-    <div>
-      <h3>{this.props.name}</h3>
-      <DescriptionWithLink descripion={this.props.descripion} link={this.props.link}/>
-      <br/>
-      <img src={this.props.imagem}></img>
-      <h4>Satellites</h4>
-      <ul>
-        {this.state.satellites.map((satellite)=>
-        <li>{satellite.name}</li>
-        )}
-      </ul>
-      <hr/>""
-    </div> 
-);
+  render() {
+    return (
+      <div>
+        <h3>{this.props.name}</h3>
+        <DescriptionWithLink
+          descripion={this.props.descripion}
+          link={this.props.link}
+        />
+        <br />
+        <img src={this.props.imagem}></img>
+        <h4>Satellites</h4>
+        <ul>
+          {this.state.satellites.map((satellite) => (
+            <li>{satellite.name}</li>
+          ))}
+        </ul>
+        <hr />
+      </div>
+    );
+  }
 }
-}
- 
+
 export default Planet;
