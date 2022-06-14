0958128382260f463cd9ada2451bf2c5ca83d7a0
WesleyDev <57929638+WesleyDev184@users.noreply.github.com>
2022-06-14 15:54:50 -0300

    melhoria

---
+++ exercicio-3 copy/src/App.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useEffect, useState } from "react";
+import Planet from "./shared/planet";
+import "./App.css";
+
+async function getPlanets() {
+  let response = await fetch("http://localhost:3000/api/planets.json");
+  let data = await response.json();
+  return data;
+}
+//  componentDidMount() {
+//      getPlanets().then((data) => {
+//        this.setState((state) => ({
+//          planets: data["planets"],
+//        }));
+//      });
+//    }
+
+const App = () => {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    getPlanets().then((data) => {
+      setPlanets(data["planets"]);
+    });
+  });
+
+  return (
+    <div className="app-body">
+      <h1>Lista de planetas</h1>
+      <hr />
+      {planets.map((planet) => (
+        <Planet
+          id={planet.id}
+          name={planet.name}
+          descripion={planet.description}
+          link={planet.link}
+          imagem={planet.imagem}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
